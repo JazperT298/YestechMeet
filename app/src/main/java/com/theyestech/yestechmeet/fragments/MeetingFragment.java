7aefb2181e595ee9023d792fbf1dcf0c48513482
jazper298 <minatosjutsu@gmail.com>
2020-08-31 10:40:09 +0800

    new meeting and join meeting UI and function

--- app/src/main/java/com/theyestech/yestechmeet/fragments/MeetingFragment.java
+++ app/src/main/java/com/theyestech/yestechmeet/fragments/MeetingFragment.java
@@ -1,5 +1,6 @@
 package com.theyestech.yestechmeet.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,57 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.material.button.MaterialButton;
 import com.theyestech.yestechmeet.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MeetingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.URL;
+
 public class MeetingFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View view;
+    private Context context;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MeetingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MeetingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MeetingFragment newInstance(String param1, String param2) {
-        MeetingFragment fragment = new MeetingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private Button btn_NewMeeting, btn_JoinMeeting;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meeting, container, false);
+        view = inflater.inflate(R.layout.fragment_meeting, container, false);
+        context = getContext();
+        initializeUI();
+        return view;
+    }
+
+    private void initializeUI(){
+        btn_NewMeeting = view.findViewById(R.id.btn_NewMeeting);
+        btn_JoinMeeting = view.findViewById(R.id.btn_JoinMeeting);
+        btn_NewMeeting.setOnClickListener(v -> {
+            try {
+                URL serverURL = new URL("https://meet.jit.si");
+                JitsiMeetConferenceOptions.Builder builder = new JitsiMeetConferenceOptions.Builder();
+                builder.setServerURL(serverURL);
+                builder.setWelcomePageEnabled(true);
+                JitsiMeetActivity.launch(getActivity(), builder.build());
+            }catch (Exception e){
+
+            }
+        });
+        btn_JoinMeeting.setOnClickListener(v -> {
+            try {
+                URL serverURL = new URL("https://meet.jit.si");
+                JitsiMeetConferenceOptions.Builder builder = new JitsiMeetConferenceOptions.Builder();
+                builder.setServerURL(serverURL);
+                builder.setWelcomePageEnabled(true);
+                JitsiMeetActivity.launch(getActivity(), builder.build());
+            }catch (Exception e){
+
+            }
+        });
     }
 }
