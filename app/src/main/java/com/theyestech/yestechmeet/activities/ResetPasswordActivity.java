801331d602de26f6c9955c1085b5cd841419f4d1
jazper298 <minatosjutsu@gmail.com>
2020-09-03 17:09:27 +0800

    reset password function

--- app/src/main/java/com/theyestech/yestechmeet/activities/ResetPasswordActivity.java
+++ app/src/main/java/com/theyestech/yestechmeet/activities/ResetPasswordActivity.java
@@ -1,16 +1,67 @@
 package com.theyestech.yestechmeet.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 import com.theyestech.yestechmeet.R;
 
 public class ResetPasswordActivity extends AppCompatActivity {
+    private View view;
+    private Context context;
+    private TextInputEditText et_email;
+    private Button btn_reset;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+        context = this;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.footerBackground));
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        firebaseAuth = FirebaseAuth.getInstance();
+        initializeUI();
+    }
+
+    private void initializeUI() {
+        et_email = findViewById(R.id.et_email);
+        btn_reset = findViewById(R.id.btn_reset);
+        btn_reset.setOnClickListener(v -> {
+            String email = et_email.getText().toString();
+
+            if (email.equals("")) {
+                Toast.makeText(ResetPasswordActivity.this, "Please input your email!", Toast.LENGTH_SHORT).show();
+            } else {
+                firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(ResetPasswordActivity.this, "Please check you Email", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                    } else {
+                        String error = task.getException().getMessage();
+                        Toast.makeText(ResetPasswordActivity.this, error, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
